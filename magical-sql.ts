a97d9945ca1e92af2734ab07a362e20e09d9653f
Александр <mysilkworld@gmail.com>
2025-10-23 17:17:27 +0300

    Version 1.0.0

--- magical-sql.ts
+++
@@ -1,8 +0,0 @@
-export class PermissionManager {
-  name: string;
-  rules: any;
-  permissions: any;
-  checkPermission(permissionName: string, entity: any): void;
-  getPermissions(entity: any): void;
-  getPermissionsFlat(entity: any): void;
-}
